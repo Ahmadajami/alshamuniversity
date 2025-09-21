682ec9273fce3a38f281902cd28fe5f8e094d591
Ahmad Alajami <ahmad_2000_aj@hotmail.com>
2025-09-22 01:00:00 +0300

    New Semister

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,66 +1,43 @@
 <script lang="ts">
 	import Header from '$lib/Header.svelte';
 	let { data } = $props();
+
 	const courseGrades = [
 		{
-			courseName: 'نظام الأسرة-الأحوال الشخصية (1)',
-			courseCode: 'RQF208 - Law',
-			credits: 2,
-			detail1: '18من 25',
-			detail2: '15 من 15',
-			sumTest: '33من 40',
-			finalTest: '39من 60',
+			courseName: 'قانون العمل و الضمان الإجتماعي',
+			courseCode: 'RQF250 - Law',
+			credits: 3,
+			detail1: '18 من 25',
+			detail2: '14 من 15',
+			sumTest: '32 من 40',
+			finalTest: '40 من 60',
 			total: '72',
-			points: 2.0,
+			points: 2.25,
 			grade: 'C+'
 		},
 		{
-			courseName: ' اللغة الانكليزية (1)',
-			courseCode: 'RQL421',
+			courseName: 'أصول الفقه',
+			courseCode: 'RQLW312',
 			credits: '3',
 			detail1: '21 من 25',
-			detail2: '14من 15',
-			sumTest: '35 من 40',
-			finalTest: '45 من 60',
-			total: '80',
-			points: 2.2,
-			grade: 'B'
-		},
-		{
-			courseName: ' اصول المحاكمات (1)',
-			courseCode: 'RQUE3',
-			credits: '2',
-			detail1: '15 من 25',
 			detail2: '15 من 15',
-			sumTest: '30من 40',
-			finalTest: '49من 60',
-			total: '79',
-			points: 2.44,
-			grade: 'B+'
-		},
-		{
-			courseName: 'مصادر الالتزام الارادية',
-			courseCode: 'RQL525',
-			credits: '3',
-			detail1: '12 من 25',
-			detail2: '15 من 15',
-			sumTest: '27 من 40',
-			finalTest: '34من 60',
-			total: '61',
-			points: 1.8,
+			sumTest: '36 من 40',
+			finalTest: '33 من 60',
+			total: '69',
+			points: 2.0,
 			grade: 'C'
 		},
 		{
-			courseName: ' قانون العقوبات العام(1) الجريمة',
-			courseCode: 'RQL312',
+			courseName: ' القانون الإداري (1)',
+			courseCode: 'RQLW-321',
 			credits: '3',
-			detail1: '18 من 25',
-			detail2: '13 من 15',
-			sumTest: '31 من 40',
-			finalTest: '51 من 60',
-			total: '82',
-			points: 2.22,
-			grade: 'B+'
+			detail1: '17 من 25',
+			detail2: '15 من 15',
+			sumTest: '32من 40',
+			finalTest: '33 من 60',
+			total: '65',
+			points: 2.0,
+			grade: 'C-'
 		}
 	];
 </script>
@@ -73,7 +50,7 @@
 <div class="mx-auto max-w-full p-4 md:max-w-screen">
 	<div class="overflow-hidden rounded-xl border border-gray-200 bg-white shadow-md">
 		<div class="border-b border-gray-100 p-4">
-			<h2 class="text-lg font-semibold text-gray-700">الفصل الثاني 2024/2025</h2>
+			<h2 class="text-lg font-semibold text-gray-700">الفصل الصيفي 2024/2025</h2>
 		</div>
 		<div class="overflow-x-auto">
 			<table class=" min-w-full border-collapse text-left">
@@ -111,7 +88,7 @@
 								dir="ltr"
 								class="border px-2 py-1 {course.grade === 'F'
 									? 'bg-red-100 text-red-600'
-									: ''} {course.grade === 'B' || course.grade === 'B+'
+									: ''} {course.grade === 'B' || course.grade === 'C+' || course.grade === 'B+'
 									? 'bg-green-100 text-green-600'
 									: ''}"
 							>
